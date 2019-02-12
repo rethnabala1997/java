79fe91d398d0c6c3b9b7341fb2541c45f3e3cdfd
rethnabala1997 <43902222+rethnabala1997@users.noreply.github.com>
2019-02-12 14:04:50 +0530

    Add files via upload

---
+++ sumnatural11.java
@@ -0,0 +1,13 @@
+public class SumNatural {
+
+    public static void main(String[] args) {
+
+        int num = 10, i = 1, sum  = 0;
+        while(i <= num)
+        {
+            sum += i;
+            i++;
+             }
+             System.out.println("Sum  = " + sum);
+    }
+}
